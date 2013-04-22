bf60ebbaa3448ff6622d2dd2fb283c4b65d5a200
Pierre FERROLLIET <pierre.ferrolliet@idci-consulting.fr>
2013-04-22 10:14:09 +0200

    Add new date range filter

---
+++ Form/Type/DateRangeType.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace IDCI\Bundle\FilterFormBundle\Form\Type;
+
+use Symfony\Component\Form\FormBuilderInterface;
+
+/**
+ * 
+ * @author:  Gabriel BONDAZ <gabriel.bondaz@idci-consulting.fr>
+ * @author:  Pierre FERROLLIET <pierre.ferrolliet@idci-consulting.fr>
+ * @licence: GPL
+ *
+ */
+class DateRangeType extends RangeType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        parent::buildForm($builder, $options);
+        $builder
+            ->add('min', 'date', array(
+                'format' => 'dd-MM-yy',
+                'widget' => 'single_text',
+            ))
+            ->add('max', 'date', array(
+                'format' => 'dd-MM-yy',
+                'widget' => 'single_text',
+            ))
+        ;
+    }
+
+    public function getName()
+    {
+        return 'date_range';
+    }
+}
